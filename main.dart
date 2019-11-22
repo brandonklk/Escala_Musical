07917934cbe378ad37746e49b256a73cdb2403c3
brandon <brandon.mkluck@gmail.com>
2019-11-22 01:07:55 -0300

    Alterando projeto e configurando pacote de testes unitarios

--- main.dart
+++ main.dart
@@ -1,63 +1,27 @@
+import 'dart:collection';
 import 'dart:io';
-
-  var scalesMajor = {
-    'C': Interval.Bigger,   
-    'D': Interval.Bigger,  
-    'E': Interval.Bigger,
-    'F': Interval.Bigger,  
-    'G': Interval.Bigger,  
-    'A': Interval.Bigger,   
-    'B': Interval.Bigger,
-  };
-
- var scalesMinor = {   
-    'Cs': Interval.Smaller,
-    'Ds': Interval.Smaller,
-    'Fs': Interval.Smaller,
-    'Gs': Interval.Smaller,
-    'As': Interval.Smaller,
-  };
-
-enum Interval { 
-   Bigger, 
-   Smaller
-}
-
-Function funcMusicalNotes(){
-   List<String> listMusicalNotas = ["C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs","A", "As", "B"];
-   print(listMusicalNotas);
-   //listMusicalNotas.forEach((x){
-   //  print(x);
-   //});
-   
-}
-
+ 
+ 
 main(){
-print("Todas as notas musicais são as seguintes");
-funcMusicalNotes();
-print("");
-
+ 
 print("Escala musical: ");
-var escala = stdin.readLineSync();
-
-  if(escala.isNotEmpty){
-    if((escala == 1) | (escala == 2)){
-      switch (escala){
-        case '1':
-          print("A escala maior é");
-          print(scalesMajor.keys);
-          break;
-
-        case '2':
-          print("A escala menor é");
-          print(scalesMinor.keys);
-          break; 
-      } 
-    }else{
-      print("Valor inserido $escala não é valido, tente 1 ou 2");
-    }
-  }else{
-    print("Nenhum valor foi preenchido");
-  }
-
+var escala = "D";//stdin.readLineSync();
+ 
+ 
+var listMusicalNotas = ["C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs","A", "As", "B"];
+   
+   
+   listMusicalNotas.forEach((index){  
+     if(index == escala){
+       
+         print(listMusicalNotas);
+         return true;
+     }else{
+         //listMusicalNotas.removeRange(index.length, escala.length);
+         listMusicalNotas.remove(index.toString());
+         var valueIndex = index.toString();
+         listMusicalNotas.add(valueIndex.toString());
+         return false;
+     }
+   });
 }
